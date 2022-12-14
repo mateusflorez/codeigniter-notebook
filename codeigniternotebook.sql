4f65b9ac6af7e2fec122a7664d1f70a4b22c15ff
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-12-14 18:44:16 -0300

    database update

--- codeigniternotebook.sql
+++ codeigniternotebook.sql
@@ -58,7 +58,6 @@ CREATE TABLE `contacts` (
   `address_id` int(11) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `website` varchar(255) NOT NULL,
-  `company_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
